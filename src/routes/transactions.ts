f5a4a4e750eb048e07b0d58f4ae4c748eac4c40f
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2023-12-12 23:33:31 -0300

    feat: create fetch specific transaction id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { z } from 'zod';
 import { randomUUID } from 'crypto';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async req => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(req.params);
+
+    const transaction = await knex('transactions').where('id', id).first();
+
+    return { transaction };
+  });
+
   app.post('/', async (req, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
