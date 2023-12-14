6182864bb0bcb756f64179a4c28252ab2976dccb
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2023-12-14 00:31:50 -0300

    refactor: change the responsability to each component

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,4 @@
-import fastify from 'fastify';
+import { app } from './app';
 import { env } from './env';
-import { transactionsRoutes } from './routes/transactions';
-import fastifyCookie from '@fastify/cookie';
-
-const app = fastify();
-
-app.register(fastifyCookie);
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-});
 
 app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running'));
