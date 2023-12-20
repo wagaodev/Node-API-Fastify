a95b4773800e4df74b8006859ca271751fc63d5c
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2023-12-20 03:09:47 -0300

    feat: adding host

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running'));
+app
+  .listen({ port: env.PORT, host: '0.0.0.0' })
+  .then(() => console.log('HTTP Server Running'));
